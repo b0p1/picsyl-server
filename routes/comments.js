a743f355c077875257cc04d6375d3bff3d2612d3
b0p1 <sofiarojo09@gmail.com>
2022-10-31 21:02:33 -0400

    get likes

--- routes/comments.js
+++
@@ -1,6 +0,0 @@
-// const router = require("express").Router();
-// const usersController = require("../controllers/commentsController");
-
-// router.route("/posts/:id/comments").get(commentsController.postComments);
-
-// module.exports = router;
--- routes/posts.js
+++ routes/posts.js
@@ -1,9 +1,11 @@
 const router = require("express").Router();
 const postsController = require("../controllers/postsController");
 const commentsController = require("../controllers/commentsController");
+const likesController = require("../controllers/likesController")
 
 router.route("/").get(postsController.index);
 router.route("/:id").get(postsController.singlePost);
 router.route("/:id/comments").get(commentsController.postComments);
+router.route("/:id/likes").get(likesController.postLikes);
 
 module.exports = router;
